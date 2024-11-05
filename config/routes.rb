8619b12bd8b9362123027154003e0d67218757d1
Yoshiki Takeshita <37509998+raithend@users.noreply.github.com>
2024-11-05 13:16:03 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
   get 'users/new'
-  root "hello#index"
 end
